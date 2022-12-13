ee103ecff1557630b57fec97667c9219e9125482
Contentcreatorz <c.oduk@outlook.com>
2022-12-13 12:24:25 +0000

    created the topics end point

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,13 +5,12 @@ const seed = require('../db/seeds/seed.js')
 const request = require('supertest')
 
 beforeEach(() => seed(data))
-
 afterAll(() => db.end && db.end())
 
 describe("GET /api/topics", () => {
-
     it("responds with an object containing an array of topic objects that have 'slug' and 'description' as properties", () => request(app).get("/api/topics")
-        .expect(200).then(({ body: { topics } }) => {
+        .expect(200)
+        .then(({ body: { topics } }) => {
             expect(topics).toHaveLength(3)
             topics.forEach(topic => {
                 expect(topic).toEqual(expect.objectContaining({
