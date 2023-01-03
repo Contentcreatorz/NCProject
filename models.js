cbca2c45580f8cbd0a06b3f9da487b04f9f30ee6
Contentcreatorz <c.oduk@outlook.com>
2023-01-03 17:45:34 +0000

    refactor select articles to show replacements at the bottom

--- models.js
+++ models.js
@@ -5,11 +5,6 @@ exports.selectTopics = () => database(`SELECT * FROM topics;`).then(({ rows: top
 exports.selectArticles = (author, topic, sortBy, order) => {
     const allowedSortBy = ['title', 'topic', 'author', 'created_at', 'votes']
     const allowedOrder = ['asc', 'desc']
-    const replacements = []
-
-    if (topic) replacements.push(topic)
-
-    if (author) replacements.push(author)
 
     if (sortBy && !allowedSortBy.includes(sortBy)) throw { status: 400, message: 'Invalid sort query' }
 
@@ -19,26 +14,31 @@ exports.selectArticles = (author, topic, sortBy, order) => {
         topic ? `articles.topic = %L` : '',
         author ? `articles.author = %L` : ''
     ].reduce((whereClause, filter) =>
-        `${whereClause} ${filter ? `${whereClause.length > 6 ? ' AND' : ''} ${filter}` : ''}`,
-        'WHERE')
-        .trim()) === 'WHERE' ? '' : ` ${processedWhereClause} `
+        `${whereClause} ${filter ? `${whereClause.length > 6 ? 'AND' : ''} ${filter}` : ''}`,
+        'WHERE').trim()) === 'WHERE' ? '' : ` ${processedWhereClause} `
+
 
     const sortClause = `
-    GROUP BY articles.article_id
-    ORDER BY articles.${sortBy || 'created_at'} ${order || 'DESC'}`
+        GROUP BY articles.article_id
+        ORDER BY articles.${sortBy || 'created_at'} ${order || 'DESC'}`
 
     const query = [''].reduce(
         baseQuery => baseQuery + whereClause + sortClause,
         `
-        SELECT articles.author, articles.article_id, articles.title, articles.topic, articles.created_at, articles.votes, 
-        CAST (COUNT(comment_id) AS INT) AS comment_count
-        FROM articles
-        LEFT JOIN comments
-        ON articles.article_id = comments.article_id`
-    );
+            SELECT articles.author, articles.article_id, articles.title, articles.topic, articles.created_at, articles.votes, 
+            CAST (COUNT(comment_id) AS INT) AS comment_count
+            FROM articles
+            LEFT JOIN comments
+            ON articles.article_id = comments.article_id`
+    )
 
-    return database(query, ...replacements).then(({ rows: articles }) => articles);
-};
+    const replacements = []
+
+    if (topic) replacements.push(topic)
+
+    if (author) replacements.push(author)
+    return database(query, ...replacements).then(({ rows: articles }) => articles)
+}
 
 exports.selectArticleById = article_id => database(`
 SELECT articles.*, CAST (COUNT(comments.comment_id) AS INT) AS comment_count
@@ -80,9 +80,9 @@ RETURNING *;`,
 exports.selectUsers = () => database(`SELECT * FROM users;`).then(({ rows: users }) => users)
 
 exports.deleteComment = (commentId) => database(`
-    DELETE FROM comments
-    WHERE comment_id = %L
-    RETURNING *`,
+DELETE FROM comments
+WHERE comment_id = %L
+RETURNING *`,
     [commentId]
 ).then(({ rows: comment }) => comment.length
     ? comment
